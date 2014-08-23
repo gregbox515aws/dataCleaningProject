c015fd6c70ef5e5080577fbac2ca0a56135e1522
Greg Field <greg78731@outlook.com>
2014-08-23 14:16:39 -0500

    In process

---
+++ run_analysis.R
@@ -0,0 +1,34 @@
+
+# This program reads the wearable computing data and does the following:
+# 
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation
+#    for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. Creates a second, independent tidy data set with the average of
+#    each variable for each activity and each subject. 
+# 
+
+# Define the list of activities
+
+1 WALKING
+2 WALKING_UPSTAIRS
+3 WALKING_DOWNSTAIRS
+4 SITTING
+5 STANDING
+6 LAYING
+
+# Read the data files into data frames
+
+testData <- read("UCI HAR Dataset/test/X_test.txt")
+testSubject <- read("UCI HAR Dataset/test/subject_test.txt")
+testActivity <- read("UCI HAR Dataset/test/y_test.txt")
+
+trainData <- read("UCI HAR Dataset/train/X_train.txt")
+trainSubject <- read("UCI HAR Dataset/train/subject_train.txt")
+trainActivity <- read("UCI HAR Dataset/train/y_train.txt")
+
+# Combine into one set of data, extracting only the columns needed
+
+
